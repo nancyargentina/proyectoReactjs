37ec97632175a47aed9f6dfd4c07db49328807f5
Nancy Martinez <zoe.arg@gmail.com>
2022-02-23 23:34:56 -0300

    adicion de datos mock

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,10 +2,10 @@ import './Item.css'
 import { Link } from "react-router-dom"
 export const Item = ( {id,nombre,desc, img, precio} )=>{
     return(
-        <div className=" col-lg-3 col-md-6 mb-4">
+        <div className=" col-lg-3 col-md-4 col-sm-6 col-6 mb-3">
             <div className="card">
                 <div className="imagen">
-                    <img className="card-img-top" src={img} alt={nombre}/>
+                    <img className="card-img-top imagen" src={img} alt={nombre}/>
                 </div>
                 <div className="card-body">
                     <h5 className="card-title">{nombre}</h5>
